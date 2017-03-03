6b64c4d9d942b856dd07e3921da4bed7cc8738e0
Iker Ibarguren <ikerib@gmail.com>
2017-03-03 14:30:16 +0100

    Extensioa hard-kodeatuta filtratzen du

--- app/js/app.js
+++ app/js/app.js
@@ -6,12 +6,18 @@ const client = new Client();
 
 client.on('hangup', function (err, body) {
     console.log('hangup:', body);
+    $('#kk').append(body);
 });
 
 client.on('newchannel', function (err, body) {
     console.log('newchannel:', body);
+    console.log($('#kk'));
+    $('#kk').append(body);
+    let niretr = "<tr><td>" + body.calleridnum +"</td><td>" + body.exten +"</td><td>" + body.event+"</td>";
+    $('#deiakTaula > tbody:last-child').append(niretr);
 });
 
 client.on('dial', function (err, body) {
     console.log('dial:', body);
+    $('#kk').append(body);
 });
--- main.js
+++ main.js
@@ -17,13 +17,11 @@ SERVER = settings.getSync('AMI.server');
 PORT = settings.getSync('AMI.port');
 USER = settings.getSync('AMI.user');
 PASSWD = settings.getSync('AMI.passwd');
-console.log(SERVER);
 
 const mb = menubar({preloadWindow: true,
                     "width" : 1000,
                     "height" : 800,
                     "index":'file://' + path.join(__dirname,"/app/index.html")});
-const express = require('express');
 const AmiIo = require("ami-io"),
     SilentLogger = new AmiIo.SilentLogger(), //use SilentLogger if you just want remove logs
     amiio = AmiIo.createClient({port:PORT, host:SERVER, login:USER, password:PASSWD,logger: SilentLogger});
@@ -33,50 +31,9 @@ const notifier = require('node-notifier');
 const Server = require('electron-rpc/server');
 const sApp = new Server();
 
+const terminalak = ['4121115060010934'];
 
-// Initialize the development socket server
-if (process.env.NODE_ENV === 'DEVELOPMENT') {
-    console.log('Initializing socket server...');
-    require('./server');
-}
-
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-function createWindow () {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-
-    // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow)
 mb.on('ready', function ready () {
-    sApp.configure(mb.window.webContents);
-
-    console.log('app is ready');
-
     // Open the DevTools.
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
         mb.webContents.openDevTools({
@@ -97,36 +54,50 @@ mb.on('ready', function ready () {
         process.exit();
     });
     amiio.on('event', function(event){
-
         if (event.event === 'Hangup'){
-            console.log('event:', event);
-            sApp.send('hangup', event);
+            // console.log(event);
+            // TODO: channel bidez bilatu behar du SIP/XXXXXXXXX-YYYYY textuan
+            let exten = event.channel.split("SIP/")[1].split("-")[0];
+            console.log(exten);
+            if ( terminalak.indexOf(exten) != -1) {
+                sApp.send('hangup', event);
+            }
         }
         if (event.event === "Dial") {
-            console.log("Ring Ring");
-            sApp.send('dial', event);
-            notifier.notify({
-                title: 'DEI BERRIA',
-                message: 'Nork: ' + event.calleridname + ' Nori: ' + event.exten,
-                icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
-                sound: true, // Only Notification Center or Windows Toasters
-                wait: true // Wait with callback, until user action is taken against notification
-            }, function (err, response) {
-                // Response is response from notification
-            });
+
+            if ( terminalak.indexOf(event.dialstring) != -1) {
+                sApp.send('dial', event);
+                notifier.notify({
+                    title: 'DEI BERRIA!',
+                    message: 'Nork: ' + event.calleridnum ,
+                    icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+                    sound: true, // Only Notification Center or Windows Toasters
+                    wait: true // Wait with callback, until user action is taken against notification
+                }, function (err, response) {
+                    // Response is response from notification
+                });
+            }
         }
         if ( event.event === "Newchannel") {
-            console.log("Channel nº 5");
-            sApp.send('newchannel', event);
+
+            if ( terminalak.indexOf(event.calleridnum) != -1 ) {
+                sApp.send('newchannel', event);
+            }
+
         }
     });
+
     amiio.connect();
-    amiio.on('connected', function(){});
+
+    amiio.on('connected', function(){
+        console.log("Conected");
+    });
+    sApp.configure(mb.window.webContents);
 });
 
 mb.on('after-create-window', function () {
     console.log("after window created");
-    mb.window.openDevTools()
+    mb.window.openDevTools();
     // mb.window.loadURL('http://www.google.es');
 });
 
@@ -146,7 +117,4 @@ app.on('activate', function () {
     //     createWindow()
     // }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
